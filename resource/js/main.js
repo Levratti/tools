11df72dc547863899d58e6e859d3d851a61886e4
Levratti S.r.l <ruslan@impresalevratti.it>
2022-04-20 17:45:11 +0200

    Update main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -3,25 +3,32 @@ var app = new Vue({
     data: {
         toolsAndScript: [
             {
-                URL: "https://levratti.github.io/tools/app/DuplicateValues.html",
+                URL: "app/DuplicateValues.html",
                 Type: "Tools",
                 Title: "Valori Duplicati",
                 LastUpdate: "20/04/2022",
                 Description: "Serve per verificare i valori duplicati tra dati di GeCo e Enel."
             },
             {
-                URL: "http://127.0.0.1:5500/app/DuplicateValues.html",
+                URL: "https://umap.geonue.com/en/map/confini-e-dati-statistici-dei-comuni-ditalia_297#14/44.8881/11.0792",
                 Type: "Tools",
-                Title: "Exsample",
+                Title: "Confini dei comuni",
                 LastUpdate: "20/04/2022",
-                Description: "Test 2"
+                Description: "Confini e dati statistici dei comuni d'Italia"
             },
             {
-                URL: "",
-                Type: "Script",
-                Title: "Exsample",
+                URL: "https://barcode.tec-it.com/it",
+                Type: "Tools",
+                Title: "Generatore codici a barre",
                 LastUpdate: "20/04/2022",
-                Description: "Test 2"
+                Description: "Genera gratis codici a barre online"
+            },
+            {
+                URL: "https://levratti.github.io/tools/script/Chiusura_RCMI_GeCo.user.js",
+                Type: "Script",
+                Title: "Chiusura RCMI GeCo",
+                LastUpdate: "20/04/2022",
+                Description: "Crea il template per la chiusura della pratica in BO."
             }
         ]
     },
