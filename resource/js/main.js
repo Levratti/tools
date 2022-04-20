4c80620f455aa1b0c2958324a7470cde7dd51be1
Levratti S.r.l <ruslan@impresalevratti.it>
2022-04-20 17:29:53 +0200

    Update main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,7 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        toolsAndScript: [
+            {
+                Link: "",
+                Type: "Tools",
+                Title: "Exsample",
+                LastUpdate: "20/04/2022",
+                Description: ""
+            }
+        ]
     },
     computed: {
 
